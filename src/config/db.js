9234a5473e9ff8985abcb91f29f8373568d1f404
monam2 <kangcw0107@gmail.com>
2025-03-19 21:16:44 +0900

    db pool 설정 및 연결

--- src/config/db.js
+++ src/config/db.js
@@ -1,12 +1,15 @@
 import pkg from "pg";
-const { Pool } = pkg;
+import dotenv from "dotenv";
 
-const pool = new pool({
-  user: process.env.USER,
-  host: process.env.HOST,
-  database: process.env.DATABASE,
-  password: process.env.PASSWORD,
-  port: process.env.DBPORT,
+const { Pool } = pkg;
+dotenv.config();
+
+const pool = new Pool({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_DATABASE,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
 });
 
 pool.on("connect", () => {
