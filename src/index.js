9efd0fda0cd3bd5cf1d41f3f7173988146bc9117
monam2 <kangcw0107@gmail.com>
2025-03-19 21:17:35 +0900

    라우터, 미들웨어, DB 풀 추가

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import pool from "./config/db.js";
+
+import userRoutes from "./routes/userRoutes.js";
+import errorHandling from "./middleware/errorHandler.js";
+import createUserTable from "./data/createUserTable.js";
 
 dotenv.config();
 
@@ -10,11 +15,22 @@ const port = process.env.PORT || 3001;
 
 // middleware
 app.use(express.json());
-app.use(cors);
+app.use(cors());
 
 // Routes
+app.use("/api", userRoutes);
 
 // Error Handling middleware
+app.use(errorHandling);
+
+// Create Table Before Starting Server
+createUserTable();
+
+// Testing postgres Connection
+app.get("/", async (req, res) => {
+  const result = await pool.query("SELECT current_database()");
+  res.send(`The database name is : ${result.rows[0]}`);
+});
 
 // Server Running
 app.listen(port, () => {
